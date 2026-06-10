2174d324955e18530726cec9d0f2496f6ea64a6b
Eisner Raphael <raphael.eisner@bischgym.eu>
2026-06-10 09:15:31 +0200

    OOP and minor changes

--- main.py
+++ main.py
@@ -39,3 +39,44 @@ def sources():
 
 if __name__ == "__main__":
     app.run(debug=True)
+
+
+
+
+
+
+
+
+# "Bauplan" definieren
+class Schwein():
+    def __init__(self):
+        # Standarddaten für Schwein definieren
+        self.hoehe = 1.05
+        self.beine = 4
+        self.lieblingsessen = ["Eier", "Karotten", "Altes Menschenessen"]
+        self.nasenloecher = 2
+        self.name = "Schweini"
+        self.freunde = 0
+        self.hunger = 10
+    
+    # Funktion um zu essen
+    def essen(self, essen):
+        # Checken, ob das Schwein das gegebene Essen mag.
+        if essen in self.lieblingsessen:
+            self.hunger -= 1
+            return "<dankbares Grunzen>"
+        else:
+            return "<wütend> Grunz Grunz!"
+
+
+# Schwein instanzieren
+schweinchen_objekt = Schwein()
+
+
+"""
+Dieses "schweinchen_objekt" hat jetzt alle oben definierten Attribute. Es hat auch die Funktion "essen".
+Es hat eine Höhe von 1.05, 4 Beine und die Lieblingsessen Eier, Karotten und Altes Menschenessen gespeichert.
+Wenn man auf diese Daten zugreifen will, macht man das über das schweinchen_objekt. 
+Wenn man das Schweinchen mit schweinchen_objekt.essen("Karotte") etwas essen lässt, würde der Hunger um einen Punkt
+weniger werden, da Karotten zu seinem Lieblingsessen gehören.
+"""
